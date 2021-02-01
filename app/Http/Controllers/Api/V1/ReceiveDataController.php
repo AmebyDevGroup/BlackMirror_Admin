9b763f1aa5626ead5ee7fa7a5f00588186e11f6d
krzysiu_1996@o2.pl <mokA4114>
2021-02-01 22:09:07 +0100

    Obsługa kamery przeniesionej do pythona

--- app/Http/Controllers/Api/V1/ReceiveDataController.php
+++ app/Http/Controllers/Api/V1/ReceiveDataController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Events\Message;
 use App\Mirror;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -18,7 +19,26 @@ class ReceiveDataController extends BaseController
                 'source' => 'temp_sensor',
                 'data' => $request->all()
             ]);
+            return response()->json([
+                'status' => 'success',
+                'message' => "Data saved"
+            ]);
+        }
+        abort(404);
+    }
 
+    public function saveCameraData(Request $request, $serial)
+    {
+        $mirror = Mirror::where('serial', $serial)->first();
+        if ($mirror) {
+            broadcast(new Message('cameraStatus', [
+                'turnOff' => $request->input('turnOff')
+            ], 'mirror.' . $serial));
+            $mirror->sensorData()->where('created_at', '<=', Carbon::now()->subDays(3))->delete();
+            $mirror->sensorData()->create([
+                'source' => 'camera_sensor',
+                'data' => $request->all()
+            ]);
             return response()->json([
                 'status' => 'success',
                 'message' => "Data saved"
