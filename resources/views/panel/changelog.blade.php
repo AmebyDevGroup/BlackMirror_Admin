db86c5138ac93e9bc8046b7b5832811f861af6ab
krzysiu_1996@o2.pl <mokA4114>
2021-02-01 22:18:59 +0100

    Poprawa wyglądu listy zmian

--- resources/views/panel/changelog.blade.php
+++ resources/views/panel/changelog.blade.php
@@ -10,23 +10,21 @@
                     <div class="darkblue-header">
                         <h5>{{ $app }}</h5>
                     </div>
-                    @foreach($commits as $commit)
-                        <div class="desc">
-                            <div class="thumb">
-                                <span class="badge bg-theme"><i class="fa fa-history"></i><muted> {{\Carbon\Carbon::parse($commit->author->date)->format('Y-m-d H:i:s')}}</muted></span>
+                    <div class="px-3 py-0">
+                        @foreach($commits as $commit)
+                            <div class="desc text-left">
+                                <div class="thumb">
+                                    <span class="badge bg-theme"><i class="fa fa-history"></i><muted> {{\Carbon\Carbon::parse($commit->author->date)->format('Y-m-d H:i:s')}}</muted></span>
+                                </div>
+                                <div class="details">
+                                    <a href="{{$commit->url}}">{{$commit->message}}</a><br/>
+                                    </p>
+                                </div>
                             </div>
-                            <div class="details">
-                                <a href="{{$commit->url}}">{{$commit->author->name}} {{$commit->message}}</a><br/>
-                                </p>
-                            </div>
-                        </div>
-                    @endforeach
+                        @endforeach
+                    </div>
                 </div>
             </div>
         @endforeach
-        <hr class="m-15">
-        <a href="https://github.com/KrzychuW/BlackMirror/commits/master" target="_blank">
-            <button class="btn btn-info btn-test">Zobacz pełną historię zmian</button>
-        </a>
     </div>
 @endsection
